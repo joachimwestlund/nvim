7ae8c52ae0bb5429f7f578c4647aa4387c4f7b50
Joachim Westlund <jocke@jocke.net>
2025-04-06 11:03:05 +0200

    Add files via upload
    First commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return { 
+	"catppuccin/nvim", 
+	name = "catppuccin", 
+    flavour = "mocha",  
+	priority = 1000,
+	config = function()
+		require('catppuccin').setup({
+			flavour = "mocha",
+        })
+		vim.cmd.colorscheme "catppuccin-mocha"
+	end
+}
